db4a6c8b7c8ea55f5efbc3490ad442eb926e95d2
roberrera <roberterrera@gmail.com>
2016-01-14 12:32:45 -0500

    Updated Car.java and created Garage class

--- src/Car.java
+++ src/Car.java
@@ -4,4 +4,12 @@ package PACKAGE_NAME;
  * Created by Ra on 1/14/16.
  */
 public class Car {
+
+    String mModelName;
+    String mColor;
+    int mTopSpeed;
+
+    public static void main(String[] args) {
+
+    }
 }
---
+++ src/Garage.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Rob on 1/14/16.
+ */
+public class Garage {
+
+    public static void main(String[] args) {
+
+    }
+}
